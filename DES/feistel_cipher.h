08ea7f97d74b980b260bc44e795a5e97debea437
bmasciotra-square-digital <brad@squaredigital.com>
2026-02-07 20:18:05 -0800

    removing declarations not needed

--- DES/feistel_cipher.h
+++ DES/feistel_cipher.h
@@ -10,19 +10,7 @@
 
 using namespace std;
 
-string substitute(string &plaintext);
-
-string initial_permutation(string &bits);
-
 string des_encrypt(string &key, string &message);
-
 string des_decrypt(string &key, string &cipher_text);
-
-vector<string> convert_binary_string_to_blocks(string &binary);
-
-string create_key(string &key);
-
-void f_box();
-
 
 #endif //A2_FEISTEL_CIPHER_H
